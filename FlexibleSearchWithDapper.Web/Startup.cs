67cb9c5a33b44361bc96c91e1a1fe269e575d329
Dan <DKMallotdaniel.mallott@gmail.comt@hotmail.com>
2020-01-03 21:34:29 -0600

    Adding Swagger

--- FlexibleSearchWithDapper.Web/Startup.cs
+++ FlexibleSearchWithDapper.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 namespace FlexibleSearchWithDapper.Web
@@ -27,6 +28,22 @@ namespace FlexibleSearchWithDapper.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Flexible Search With Dapper", 
+                    Version = "v1",
+                    Description = "An example of a flexible search API using Dapper",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Dan Mallott",
+                        Email = "daniel.mallott@gmail.com",
+                        Url = new Uri("https://twitter.com/danielmallott")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +57,9 @@ namespace FlexibleSearchWithDapper.Web
             app.UseHttpsRedirection();
 
             app.UseSerilogRequestLogging();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flexible Search With Dapper"); });
 
             app.UseRouting();
 
